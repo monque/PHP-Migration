30c844a8921649aa4e00e2b0ec03ebb7eb4c24bd
Yuchen Wang <phobosw@gmail.com>
2014-11-19 16:51:43 +0800

    Extract keyword reserved check into abstract class

---
+++ src/Changes/AbstractKeywordReserved.php
@@ -0,0 +1,59 @@
+<?php
+namespace PhpMigration\Changes;
+
+/**
+ * @author Yuchen Wang <phobosw@gmail.com>
+ *
+ * Code is compliant with PSR-1 and PSR-2 standards
+ * http://www.php-fig.org/psr/psr-1/
+ * http://www.php-fig.org/psr/psr-2/
+ */
+
+use PhpMigration\Change;
+use PhpMigration\SymbolTable;
+use PhpMigration\Utils\ParserHelper;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Stmt;
+
+abstract class AbstractKeywordReserved extends Change
+{
+    protected $tableLoaded = false;
+
+    protected $wordTable;
+
+    public function prepare()
+    {
+        if (!$this->tableLoaded) {
+            $this->wordTable = new SymbolTable(array_flip($this->wordTable), SymbolTable::IC);
+            $this->tableLoaded = true;
+        }
+    }
+
+    public function leaveNode($node)
+    {
+        /**
+         * {Description}
+         * These words have special meaning in PHP. Some of them represent
+         * things which look like functions, some look like constants, and so
+         * on - but they're not, really: they are language constructs. You
+         * cannot use any of the following words as constants, class names,
+         * function or method names. Using them as variable names is generally
+         * OK, but could lead to confusion.
+         *
+         * {Reference}
+         * http://php.net/manual/en/reserved.keywords.php
+         */
+        $name = null;
+        if ($node instanceof Stmt\Class_ || $node instanceof Stmt\Interface_ ||
+                $node instanceof Stmt\Function_ || $node instanceof Stmt\ClassMethod ||
+                $node instanceof Expr\MethodCall || $node instanceof Expr\StaticCall) {
+            $name = $node->name;
+        } elseif ($node instanceof Expr\ConstFetch ||
+                ($node instanceof Expr\FuncCall && !ParserHelper::isDynamicCall($node))) {
+            $name = $node->name->toString();
+        }
+        if (!is_null($name) && $this->wordTable->has($name)) {
+            $this->addSpot('FATAL', true, 'Keyword '.$name.' is reserved');
+        }
+    }
+}
--- src/Changes/v5dot4/IncompReserved.php
+++ src/Changes/v5dot4/IncompReserved.php
@@ -9,55 +9,22 @@ namespace PhpMigration\Changes\v5dot4;
  * http://www.php-fig.org/psr/psr-2/
  */
 
-use PhpMigration\Change;
-use PhpMigration\SymbolTable;
-use PhpMigration\Utils\ParserHelper;
-use PhpParser\Node\Expr;
-use PhpParser\Node\Stmt;
+use PhpMigration\Changes\AbstractKeywordReserved;
 
-class IncompReserved extends Change
+class IncompReserved extends AbstractKeywordReserved
 {
     protected static $version = '5.4.0';
 
-    protected static $prepared = false;
-
-    protected static $keywords = array(
+    /**
+     * {Description}
+     * The following keywords are now reserved, and may not be used as names by
+     * functions, classes, etc.
+     * trait, callable, insteadof
+     *
+     * {Reference}
+     * http://php.net/manual/en/migration54.incompatible.php
+     */
+    protected $wordTable = array(
         'trait', 'callable', 'insteadof'
     );
-
-    public function prepare()
-    {
-        if (!static::$prepared) {
-            static::$keywords = new SymbolTable(array_flip(static::$keywords), SymbolTable::IC);
-            static::$prepared = true;
-        }
-    }
-
-    public function leaveNode($node)
-    {
-        /**
-         * {Description}
-         * The following keywords are now reserved, and you cannot use any of
-         * the following words as constants, class names, function or method
-         * names. Using them as variable names is generally OK, but could lead
-         * to confusion.
-         * trait, callable, insteadof
-         *
-         * {Reference}
-         * http://php.net/manual/en/reserved.keywords.php
-         * http://php.net/manual/en/migration54.incompatible.php
-         */
-        $name = null;
-        if ($node instanceof Stmt\Class_ || $node instanceof Stmt\Interface_ ||
-                $node instanceof Stmt\Function_ || $node instanceof Stmt\ClassMethod ||
-                $node instanceof Expr\MethodCall || $node instanceof Expr\StaticCall) {
-            $name = $node->name;
-        } elseif ($node instanceof Expr\ConstFetch ||
-                ($node instanceof Expr\FuncCall && !ParserHelper::isDynamicCall($node))) {
-            $name = $node->name->toString();
-        }
-        if (!is_null($name) && static::$keywords->has($name)) {
-            $this->addSpot('FATAL', true, 'Keyword '.$name.' is reserved');
-        }
-    }
 }
