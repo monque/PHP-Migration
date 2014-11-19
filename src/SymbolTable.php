2385d4457d424736b816c121545e2f12ab6f998b
Yuchen Wang <phobosw@gmail.com>
2014-11-19 17:16:26 +0800

    Improved key preparation, add delete method

--- src/SymbolTable.php
+++ src/SymbolTable.php
@@ -28,10 +28,10 @@ class SymbolTable implements \Iterator
         $this->caseSensitive = $case_sensitive;
     }
 
-    public function has($key)
+    protected function prepareKey(&$key)
     {
         // Compatible with almost all type key
-        if (method_exists($key, '__toString')) {
+        if (is_object($key) && method_exists($key, '__toString')) {
             $key = (string) $key;
         }
         if (!is_string($key)) {
@@ -39,19 +39,48 @@ class SymbolTable implements \Iterator
         }
 
         $key = $this->caseSensitive ? $key : strtolower($key);
+        return true;
+    }
+
+    public function has($key)
+    {
+        if (!$this->prepareKey($key)) {
+            return false;
+        }
+
         return isset($this->data[$key]);
     }
 
     public function get($key)
     {
-        $key = $this->caseSensitive ? $key : strtolower($key);
+        if (!$this->prepareKey($key)) {
+            return null;
+        }
+
         return isset($this->data[$key]) ? $this->data[$key] : null;
     }
 
     public function set($key, $value)
     {
-        $key = $this->caseSensitive ? $key : strtolower($key);
+        if (!$this->prepareKey($key)) {
+            return null;
+        }
+
         return $this->data[$key] = $value;
+    }
+
+    public function del($key)
+    {
+        if (!$this->prepareKey($key)) {
+            return false;
+        }
+
+        if (!isset($this->data[$key])) {
+            return false;
+        }
+
+        unset($this->data[$key]);
+        return true;
     }
 
     // Iterator
