dd5bdcca5ea4baf4dfafd7ad677186d0da041ba6
Paulo Vitor Lima <paulovitorbal@gmail.com>
2017-10-02 19:30:52 -0300

    making CI happy again

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,3 +2,9 @@
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 $loader->addPsr4('PhpMigration\\', __DIR__);
+
+$oldClass = '\PHPUnit\Framework\TestCase';
+$newClass = '\PHPUnit_Framework_TestCase';
+if (!class_exists($newClass) && class_exists($oldClass)) {
+    class_alias($oldClass, $newClass);
+}
